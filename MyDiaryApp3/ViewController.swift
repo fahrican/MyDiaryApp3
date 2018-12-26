fa0ca95a0129844f281966f100286a95e34872d8
Fahri MacBook <fahri-can@web.de>
2018-12-26 20:46:43 +0100

    Initial Commit

---
+++ MyDiaryApp3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyDiaryApp3
+//
+//  Created by fahri on 26.12.18.
+//  Copyright © 2018 fahri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
