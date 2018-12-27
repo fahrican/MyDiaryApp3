5b1ec5ba2c5a32baaba41905f5690c39a65bb63e
Fahri MacBook <fahri-can@web.de>
2018-12-27 01:22:38 +0100

    Finish implementing second entity Day in My Day.

--- MyDiaryApp3/TaskViewController.swift
+++ MyDiaryApp3/TaskViewController.swift
@@ -16,12 +16,49 @@ class TaskViewController: UIViewController {
     
     @IBOutlet weak var myToDos: UITextView!
     
+    @IBOutlet weak var myDays: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         self.myToDos.text = ""
+        self.myDays.text = ""
     }
 
+    @IBAction func addDayButton(_ sender: Any) {
+        
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+        
+        let managedContext = appDelegate.persistentContainer.viewContext
+        
+        let dayEntity = NSEntityDescription.entity(forEntityName: "Day", in: managedContext)!
+        
+        let day = NSManagedObject(entity: dayEntity, insertInto: managedContext)
+        
+        day.setValue(self.userInput.text, forKey: "day_name")
+        
+        do {
+            try managedContext.save()
+        } catch let error as NSError {
+            print("Error: \(error), \(error.userInfo)")
+        }
+        
+        // auslesen
+        let dayFetch = NSFetchRequest<NSFetchRequestResult>(entityName: "Day")
+        let days = try! managedContext.fetch(dayFetch)
+        
+        var dayNames = ""
+        
+        for case let day as Day in days {
+            dayNames = dayNames + day.day_name! + "\n"
+        }
+        self.myDays.text = dayNames
+        
+        self.userInput.text = ""
+    }
+    
     @IBAction func addTextButton(_ sender: Any) {
         
         guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
